0ed84ef92e1ecb3527ce5195462a8c61368aff05
srezasm <srezas.2017@gmail.com>
2021-03-18 22:21:53 +0330

    foreground and background color adjusted

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using static System.Console;
+//using static System.Console;
 namespace AutoProjectRenamer
 {
    class Program
    {
+      public static void WriteLine(string buffer, ConsoleColor foreground = ConsoleColor.DarkGreen, ConsoleColor backgroundColor = ConsoleColor.Black)
+      {
+         Console.ForegroundColor = foreground;
+         Console.BackgroundColor = backgroundColor;
+         Console.WriteLine(buffer);
+         Console.ResetColor();
+      }
+
+      public static string ReadLine()
+      {
+         var line = Console.ReadLine();
+         return line ?? string.Empty;
+      }
+
       static void Main(string[] args)
       {
          bool doWhile = true;
