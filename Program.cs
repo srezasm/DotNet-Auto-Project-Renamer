29af4bd13fd85ccdb997769cc3ec4bbb84dc30cc
srezasm <srezas.2017@gmail.com>
2021-03-18 08:00:38 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoProjectRenamer
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
